1a76b3476689c9635a99f8d1903fea2352d1017f
David Stanger <loonydadtoony@gmail.com>
2025-06-05 20:34:26 -0600

    everything but the venv libraries and the service manifest im still working

--- python/weather.py
+++ python/weather.py
@@ -21,6 +21,9 @@ class Weather:
       self.latitude = latitude
       self.longitude = longitude
 
+      units = self._get_units()
+      self.units = units
+
   def _session(self) -> requests.Session:
     session = requests.Session()
     session.verify = False
@@ -35,18 +38,41 @@ class Weather:
       latitude = ""
       longitude = ""
       coordinates_url= f"http://api.openweathermap.org/geo/1.0/zip?zip={self.args.zip},{self.args.country}&appid={self.api_key}"
-
+      print(coordinates_url) 
       response = self._get(coordinates_url)
       coordinates = response.json()
 
-      latitude = coordinates['ltu']
-      longitude = coordinates['lgn']
-
+      latitude = coordinates['lat']
+      longitude = coordinates['lon']
       return latitude, longitude
 
+  def _get_units(self):
+      units = "standard"
+      return units
+
+  def get_weather_details(self) -> str:
+      weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&units={self.args.units}&appid={self.api_key}"
+      response = self._get(weather_url)
+
+      weather_details = ""
+      weather_deets = response.json()
+      my_dict = {"Temperature": weather_deets['main']['temp'], "Temperature Min": weather_deets['main']['temp_min'],
+                 "Temperature Max": weather_deets['main']['temp_max'], "Wind Speed": weather_deets['wind']['speed']}
+
+      for key, value in my_dict.items():
+          weather_details += key + ": " + str(value) + "\n"
+
+      my_array = weather_deets['weather']
+
+      for element in my_array:
+          weather_details += "Description: " + element['description'] + "\n"
+
+      return weather_details
+
   def get_daily_weather(self):
-    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&appid={self.api_key}"
-    self.get(weather_url)
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&units={self.args.units}&appid={self.api_key}"
+    self._get(weather_url)
+
 
 if __name__ == "__main__":
 
@@ -57,7 +83,9 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description= 'Generates current weather data')
   parser.add_argument('--zip', type=str, help="enter zipcode for weather data", required=True)
   parser.add_argument('--country', '-c', type=str, default='US', help="enter country abbreviation")
+  parser.add_argument('--units', '-u', type=str, help="enter a measurement standard: standard, metric or imperial")
   args = parser.parse_args()
 
   weather = Weather(args)
   weather.get_daily_weather()
+  print (weather.get_weather_details())
